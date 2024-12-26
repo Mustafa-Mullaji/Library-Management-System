2b9901db219b8f861b2e122171b61f0a3e22ce76
Mustafa <mustafamullaji53@gmail.com>
2024-12-26 18:45:44 +0530

    ISBN Contraints Implemented

--- library.py
+++ library.py
@@ -1,5 +1,7 @@
 class Book:
     def __init__(self, isbn, title, author, publication_year):
+        if not isbn.isdigit() or len(isbn) != 5:
+            raise ValueError("ISBN must be a 5-digit number.")
         self.isbn = isbn
         self.title = title
         self.author = author
