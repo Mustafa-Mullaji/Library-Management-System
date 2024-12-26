c1257a7bc002b9a029cd9cf42c7f87721e459b99
Mustafa <mustafamullaji53@gmail.com>
2024-12-26 19:00:54 +0530

    testing unique ISBN (test fails)

--- test_library.py
+++ test_library.py
@@ -24,6 +24,14 @@ class TestLibrary(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             Book("123", "Short ISBN Book", "Author", 2000)
         self.assertEqual(str(context.exception), "ISBN must be a 5-digit number.")
+    
+    def test_add_book_unique_isbn(self):
+         library = Library()
+         book1 = Book("12345", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+         book2 = Book("12345", "1984", "George Orwell", 1949)
+         library.add_book(book1)
+         with self.assertRaises(ValueError):  
+          library.add_book(book2)
 
 
 if __name__ == "__main__":
