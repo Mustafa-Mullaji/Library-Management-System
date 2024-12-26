64f5095eb3a93c096551683da3efb2402fe014d9
Mustafa <mustafamullaji53@gmail.com>
2024-12-26 17:57:29 +0530

    initializing library and book classes

---
+++ library.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, isbn, title, author, publication_year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+        self.is_borrowed = False
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
